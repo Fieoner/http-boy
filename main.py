292462202469c76b6b09ef177a024a744992b30e
Dani <contact@daniguardiola.me>
2018-08-24 18:26:13 +0200

    Removed deprecated code

--- main.py
+++ main.py
@@ -44,29 +44,6 @@ class GBHTTPRequestHandler(BaseHTTPRequestHandler):
                 json_data = json.dumps(data, separators=(',',':'))
                 self.wfile.write(json_data)
                 return
-            # if path == '/start':
-            #     emulator.gamestart(pyboy)
-            #     self.send_response(200)
-            #     self.send_header('Content-type', 'text-json')
-            #     self.end_headers()
-            #     ss_image = getScreenshot(pyboy)
-            #     buf = cStringIO.StringIO()
-            #     ss_image.save(buf, format="PNG")
-            #     ss_image.save('test.png')
-            #     base64string = base64.b64encode(buf.getvalue())
-            #
-            #     data = {}
-            #     data['result'] = {
-            #         "type": 'simple',
-            #         "screenshot": base64string
-            #     }
-            #
-            #     data['options'] = {
-            #         "type": 'controller'
-            #     }
-            #     json_data = json.dumps(data, separators=(',',':'))
-            #     self.wfile.write(json_data)
-            #     return
 
             if path == '/execute':
                 key = query["key"][0]
