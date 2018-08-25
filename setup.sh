0afac3eac0109998ae24162ac1cfc52009ed5e66
Cesar Rojas <cesar.fieoner@gmail.com>
2018-08-25 16:31:11 +0200

    setup script is not working lol

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! $UID -eq 0 ]
+then
+	echo "script must be run as root"
+	exit 1
+fi
+
+apt update
+apt -y install git pypy pypy-dev virtualenv libsdl2-dev libjpeg-dev
+
+virtualenv . -p `which pypy`
+source ./bin/activate
+
+pip install git+https://bitbucket.org/pypy/numpy.git
+pip install git+https://github.com/marcusva/py-sdl2.git
+
+
